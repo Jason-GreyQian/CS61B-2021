f2bbe2cffd6577b11dc93ff2c1945f6ae04ddb66
Jason-GreyQian <jason20011018@gmail.com>
2024-11-20 19:33:03 +0800

    add the Timing test for SLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,4 +1,6 @@
 package timingtest;
+
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -23,6 +25,37 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<Integer>();
+        AList<Double> times = new AList<Double>();
+        AList<Integer> opCounts = new AList<Integer>();
+
+        // How many times has the experiment been conducted
+        int nTimes = 8;
+        int nOpts = 1000;
+        int[] nArray = new int[nTimes];
+        for (int i = 0; i < nTimes; i++) {
+            nArray[i] = (int) (Math.pow(2, i) * 1000);
+        }
+
+        for (int i = 0; i < nTimes; i++) {
+            int N = nArray[i];
+            SLList<Integer> list = new SLList<Integer>();
+            for (int j = 0; j < N; j++) {
+                list.addLast(j);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < nOpts; j++) {
+                list.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+
+            Ns.addLast(N);
+            times.addLast(timeInSeconds);
+            opCounts.addLast(nOpts);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
