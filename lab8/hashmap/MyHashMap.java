a1318fb44d27b9253e4c0a936055dc9bca11e42d
Jason-GreyQian <jason20011018@gmail.com>
2024-12-17 13:49:32 +0800

    the ugry version of the hashMao

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -28,11 +28,20 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    private int size;
+    private int bucketSize;
+    private double loadFactor;
+    private int REFACTOR = 2;
+    private Set<K> keySet;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(16, 0.75);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize, 0.75);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +50,22 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        loadFactor = maxLoad;
+        bucketSize = initialSize;
+        keySet = new HashSet<K>();
+        buckets = new Collection[bucketSize];
+        size = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = createBucket();
+        }
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
@@ -69,7 +87,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<Node>();
     }
 
     /**
@@ -87,5 +105,193 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+
+    /**
+     * Removes all of the mappings from this map.
+     */
+    @Override
+    public void clear() {
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i].clear();
+        }
+        size = 0;
+        keySet.clear();
+    }
+
+    /**
+     * Returns true if this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        int bucketIndex = calBucketNumbers(key);
+        boolean contains = false;
+        for (Node node : buckets[bucketIndex]) {
+            if (node.key.equals(key)) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        int bucketIndex = calBucketNumbers(key);
+        V value = null;
+        for (Node node : buckets[bucketIndex]) {
+            if (node.key.equals(key)) {
+                value = node.value;
+                break;
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key,
+     * the old value is replaced.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        int bucketIndex = calBucketNumbers(key);
+        if (containsKey(key)) {
+            for (Node node : buckets[bucketIndex]) {
+                if (node.key.equals(key)) {
+                    node.value = value;
+                }
+            }
+        } else {
+            size += 1;
+            keySet.add(key);
+            buckets[bucketIndex].add(createNode(key, value));
+        }
+
+        if (isOverload()) {
+            increaseBucketSize();
+        }
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map.
+     */
+    @Override
+    public Set<K> keySet() {
+        return keySet;
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+       if (!containsKey(key)) {
+           return null;
+       } else {
+           size -= 1;
+           int bucketIndex = calBucketNumbers(key);
+           V value = null;
+           for (Node node : buckets[bucketIndex]) {
+               if (node.key.equals(key)) {
+                   value = node.value;
+                   buckets[bucketIndex].remove(node);
+                   keySet.remove(key);
+                   break;
+               }
+           }
+           return value;
+       }
+    }
+
+    /**
+     * Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public V remove(K key, V value) {
+        if (!containsKey(key)) {
+            return null;
+        } else {
+            int bucketIndex = calBucketNumbers(key);
+            for (Node node : buckets[bucketIndex]) {
+                if (node.key.equals(key)) {
+                    if (node.value == value) {
+                        buckets[bucketIndex].remove(node);
+                        size -= 1;
+                        keySet.remove(key);
+                        return value;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+    /** Calculate the bucket index based the key. */
+    private int calBucketNumbers(K key) {
+        int hashCode = key.hashCode();
+        return Math.floorMod(hashCode, bucketSize);
+    }
+
+    /** if overload should resize the bucket. */
+    private void increaseBucketSize() {
+        // init the new bucket
+        bucketSize *= REFACTOR;
+        Collection<Node>[] newBuckets = new Collection[bucketSize];
+        for (int i = 0; i < bucketSize; i++) {
+            newBuckets[i] = createBucket();
+        }
+
+        // move the node to new bucket
+        for (int i = 0; i < buckets.length; i++) {
+            for (Node node : buckets[i]) {
+                int newBucketIndex = calBucketNumbers(node.key);
+                newBuckets[newBucketIndex].add(node);
+            }
+        }
+
+        buckets = newBuckets;
+    }
+
+    /** Check weather overloads. */
+    private boolean isOverload() {
+        double loadFactorNow = 1.0 * size / bucketSize;
+        return loadFactorNow > loadFactor;
+    }
+
 
 }
